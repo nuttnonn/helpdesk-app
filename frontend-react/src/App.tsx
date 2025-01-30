0ae7620f3e93fe36a597d5356ee10a0762373546
nuttnonn <nutt.nonn@gmail.com>
2025-01-31 02:50:40 +0700

    chore(frontend): install TailwindCSS v3 with autoprefixer

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div className="w-screen h-screen flex flex-col items-center justify-center">
+        <div className="w-screen h-screen flex flex-col items-center justify-center text-blue-400">
             Hello World!
         </div>
     )
